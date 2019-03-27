5336385b0a111ad5d4dae4eef7a0a22582fe7068
384665440 <384665440@qq.com>
2019-03-27 15:27:03 +0800

    更新了js逻辑

--- SpongeBob_Mall/Scripts/abc.js
+++ SpongeBob_Mall/Scripts/abc.js
@@ -1,32 +1,39 @@
-var ranl = 0;
-    var useRand = 0;
-    images = new Array;
-	images[1] = new Image();
-    images[1].src ="../img/d1.jpg";
-	images[2] = new Image();
-	images[2].src = "../img/d2.png";
-	images[3] = new Image();
-	images[3].src = "../img/d3.png";
-	function swapPic(){
-	   var imgnum = images.length-1;
-       do{
-	    var randnum = Math.random();
-		randl = Math.round((imgnum-1)*randnum)+1;
-	   }while(randl==useRand);
-        useRand = randl;
-        document.randimg.src = images[useRand].src		
-		setTimeout('swapPic()',2000);
-}
-
-function validate_required(field, _alerttxt) {
+function validate_required(field, alerttxt) {
     with (field) {
-        if (value == null || value == "") { alert(alerttxt); return false }
-        else { return true }
+        if (value == null || value == "") {
+            placeholder = alerttxt;
+            return false;
+        }
+        else { return true; }
     }
 }
 
 function validate_form(thisform) {
     with (thisform) {
-        if (validate_required(username, "Username must be filled out!") == false) { username.focus(); return false }
+        var bl = true;
+        bl = validate_required(username, "用户名不能为空") && bl;
+        bl = validate_required(password, "密码不能为空") && bl;
+        if (!bl) {
+            return false;
+        }
     }
+}
+var ranl = 0;
+var useRand = 0;
+images = new Array;
+images[1] = new Image();
+images[1].src = "../img/d1.jpg";
+images[2] = new Image();
+images[2].src = "../img/d2.png";
+images[3] = new Image();
+images[3].src = "../img/d3.png";
+function swapPic() {
+    var imgnum = images.length - 1;
+    do {
+        var randnum = Math.random();
+        randl = Math.round((imgnum - 1) * randnum) + 1;
+    } while (randl == useRand);
+    useRand = randl;
+    document.randimg.src = images[useRand].src
+    setTimeout('swapPic()', 2000);
 }
