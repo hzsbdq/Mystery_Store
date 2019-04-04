4b989749b6350398033ae4e124a0347152a8dad6
384665440 <384665440@qq.com>
2019-04-04 16:42:07 +0800

    更新了背包页面的后台逻辑：分类查询，排序，分页（未测试）

--- SpongeBob_Mall/Controllers/XieBaoWang/BagController.cs
+++ SpongeBob_Mall/Controllers/XieBaoWang/BagController.cs
@@ -14,14 +14,120 @@ namespace SpongeBob_Mall.Controllers.XieBaoWang
     {
         private MySqlContext db = new MySqlContext();
         // 获取背包列表
-        public async Task<ActionResult> Index()
+        public async Task<ActionResult> Index(int? fl,string choose,int? time_sort,int? price_sort,int? change_page)
         {
-            //根据位置排序
             User user = (User)HttpContext.Session["user"];
-            List<Goods> goods = await db.Goods.Where(b => b.UserID == user.UserId && b.State==0).OrderBy(b => b.Location).ToListAsync();
-            return View(goods);
+            List<Goods> goods;
+            IQueryable<Goods> goods_or;
+            IOrderedQueryable<Goods> goods_ored;
+            int bag_page = 0;                             //初始化背包页面的值
+            if (HttpContext.Session["bag_page"] == null)  //如果session中没有值，初始化值
+            {
+                HttpContext.Session["bag_page"] = 0;
+            }
+            if (change_page != null)//如果是页操作，更新背包页的值
+            {
+                bag_page = (int)((int)HttpContext.Session["bag_page"]+change_page);
+            }
+            else//否则将背包页的值归零
+            {
+                bag_page = 0;
+            }
+            if (bag_page < 1)//如果是非法操作，归零
+            {
+                HttpContext.Session["bag_page"] = 0;
+            }
+            else
+            {
+                HttpContext.Session["bag_page"] = bag_page;
+            }
+            HttpContext.Session["time_sort"] = (HttpContext.Session["time_sort"] == null|| time_sort == null)&&change_page==null ? 0 : time_sort;
+            HttpContext.Session["price_sort"] = (HttpContext.Session["price_sort"] == null || price_sort == null)&&change_page==null ? 0 : price_sort;
+            int time_sort_clone = (int)HttpContext.Session["time_sort"];
+            int price_sort_clone = (int)HttpContext.Session["price_sort"];
+            if (time_sort_clone != 0)
+            {
+                price_sort = 0;
+            }
+            if(price_sort_clone != 0)
+            {
+                time_sort = 0;
+            }
 
-            //return View();
+            if ((fl == null || choose == null)&&time_sort==null&&price_sort==null&&change_page==null)
+            {
+                HttpContext.Session["rare"] = "不限";
+                HttpContext.Session["type"] = "不限";
+            }
+            else if (fl == 1 && time_sort == null && price_sort == null && change_page == null)
+            {
+                HttpContext.Session["rare"] = choose;
+            }
+            else if(fl ==2 && time_sort == null && price_sort == null && change_page == null)
+            {
+                HttpContext.Session["type"] = choose;
+            }
+            string rare = (string)HttpContext.Session["rare"];
+            string type = (string)HttpContext.Session["type"];
+            if (rare != "不限" && type != "不限")
+            {
+                goods_or = db.Goods.Where(b => b.UserID == user.UserId && b.State == 0 && b.Map.Rare == rare && b.Map.type == type);
+            }
+            else if (rare != "不限")
+            {
+                goods_or = db.Goods.Where(b => b.UserID == user.UserId && b.State == 0 && b.Map.Rare == rare);
+            }
+            else if(type != "不限")
+            {
+                goods_or = db.Goods.Where(b => b.UserID == user.UserId && b.State == 0 && b.Map.type == type);
+            }
+            else
+            {
+                goods_or = db.Goods.Where(b => b.UserID == user.UserId && b.State == 0);
+            }
+
+            if (time_sort_clone != 0)
+            {
+                if (time_sort_clone == 1)
+                {
+                    goods_ored = goods_or.OrderBy(b => b.GetDate);
+                }
+                else
+                {
+                    goods_ored = goods_or.OrderByDescending(b => b.GetDate);
+                }
+            }
+            else
+            {
+                if (price_sort_clone == 1)
+                {
+                    goods_ored = goods_or.OrderBy(b => b.Price);
+                }
+                else
+                {
+                    goods_ored = goods_or.OrderByDescending(b => b.Price);
+                }
+            }
+
+            try//防止越界
+            {
+                goods = await goods_ored.Skip(8 * (bag_page - 1)).Take(8).ToListAsync();
+            }
+            catch(Exception e)
+            {
+                goods = await goods_ored.Skip(0).Take(8).ToListAsync();
+            }
+
+            if (goods.Count < 8)
+            {
+                HttpContext.Session["bag_max_page"] = bag_page;
+            }
+            else
+            {
+                HttpContext.Session["bag_max_page"] = bag_page+1;
+            }
+
+            return View(goods);
         }
 
         // 丢弃物品
