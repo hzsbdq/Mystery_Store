3800713771c3dfa0d74af039bf2206b93ec26305
384665440 <384665440@qq.com>
2019-05-24 22:58:02 +0800

    完成随机抽奖小游戏（随机还存在问题）

---
+++ SpongeBob_Mall/Controllers/RollGoodsController.cs
@@ -0,0 +1,62 @@
+﻿using SpongeBob_Mall.DAL;
+using SpongeBob_Mall.Models;
+using SpongeBob_Mall.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpongeBob_Mall.Controllers
+{
+    public class RollGoodsController : Controller
+    {
+        private MySqlContext db = new MySqlContext();
+        private Roll roll;
+        private List<Map> maps;
+
+        public RollGoodsController()
+        {
+            roll = new Roll(db);
+        }
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> Roll()
+        {
+            User user = (User)HttpContext.Session["user"];
+            if (user.Property < 1000)
+            {
+                return Redirect("Index");
+            }
+            maps = new List<Map>();
+            for (int i = 0; i < 10; i++)
+            {
+                maps.Add(await roll.StartRoll());
+            }
+            foreach (Map map in maps)
+            {
+                Goods goods = new Goods
+                {
+                    UserID = user.UserId,
+                    MapID = map.MapId,
+                    Amount = 1,
+                    Price = 0,
+                    GetDate = DateTime.Now,
+                    CollectionTag = 0,
+                    State = 0,
+                    Location = 1
+                };
+                db.Goods.Add(goods);
+            }
+            db.Users.Attach(user);
+            user.Property -= 1000;
+            await db.SaveChangesAsync();
+            return View("~/Views/RollGoods/Index.cshtml", maps);
+        }
+    }
+}
---
+++ SpongeBob_Mall/Tools/Roll.cs
@@ -0,0 +1,57 @@
+﻿using SpongeBob_Mall.DAL;
+using SpongeBob_Mall.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SpongeBob_Mall.Tools
+{
+    public class Roll
+    {
+        private MySqlContext db;
+        int num;
+        int num2;
+        string type;
+        List<Map> maps;
+        public Roll(MySqlContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<Map> StartRoll()
+        {
+            num = ToRoll(1, 100);
+            if (num == 16)
+            {
+                type = "至宝";
+            }else if (num >= 10 && num <= 15)
+            {
+                type = "神话";
+            }
+            else if (num >= 1 && num <= 9)
+            {
+                type = "不朽";
+            }
+            else if (num >= 17 && num <= 35)
+            {
+                type = "稀有";
+            }
+            else
+            {
+                type = "普通";
+            }
+            maps = await db.Maps.Where(b => b.Rare == type).ToListAsync();
+            num2 = ToRoll(0, maps.Count);
+            return maps[num2];
+        }
+
+        public int ToRoll(int a,int b)
+        {
+            Random ran = new Random(System.DateTime.Now.Millisecond+num+num2);
+            return ran.Next(a, b);
+        }
+    }
+}
